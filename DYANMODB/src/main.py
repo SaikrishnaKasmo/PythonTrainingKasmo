b983c8208655a3d719ea7bab959c5442ab9d4a09
SaikrishnaKasmo <saikrishna.kadupudi@kasmodigital.com>
2025-09-19 00:10:53 -0500

    Update main.py

--- DYANMODB/src/main.py
+++ DYANMODB/src/main.py
@@ -11,9 +11,6 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from config.config import *
 # Initialize DynamoDB client
 session = boto3.Session(
-    aws_access_key_id="AKIA4OLIY6GTVLIT2CRN",
-    aws_secret_access_key="mbTcb4JtXqK8Xj2wEL+/uHD83reFjcO5h8nPFAne",
-    region_name="us-east-2"   # change to your region
 aws_access_key_id=aws_details["aws_access_key_id"],
     aws_secret_access_key=aws_details["aws_secret_access_key"],
     region_name=aws_details["region_name"]
